5da9143a4267a680e60ae64f2bf777f745ccdea1
chenglong.li <chenglong.li007@gmail.com>
2023-12-18 20:01:49 +0800

    add code for transfer transaction and according test

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
@@ -13,4 +17,79 @@ func NewStore(db *sql.DB) *Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	//	fmt.Println("Enter execTx function")
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		//		fmt.Println("Create Tx object fails")
+		return err
+	}
+	//	fmt.Println("Create Tx object successfully")
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	//	fmt.Println("Execute transaction in store.go")
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		//		fmt.Println("Execute CreateTransfer in store.go")
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		//		fmt.Println("Execute create Transfer successfully")
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		//		fmt.Println("Execute create fromEntry successfully")
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		//		fmt.Println("Execute create toEntry successfully")
+
+		return nil
+	})
+
+	return result, err
 }
